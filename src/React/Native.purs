739127ba09440d9d78e35fee222ede4c0bf1b197
Arthur Xavier <arthur.xavierx@gmail.com>
2016-07-07 17:05:01 -0300

    feat(React.Native): add createElement function
    The createElement function had to be redefined in this package because the current version of
    React.createElement does not convert the expected props parameter to an object when calling the
    foreign createElement function.

--- src/React/Native.purs
+++ src/React/Native.purs
@@ -1,11 +1,12 @@
 module React.Native
   ( PLATFORM
   , registerComponent
+  , createElement
   ) where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import React (ReactClass)
+import React (ReactClass, ReactElement)
 
 -- | This effect represents computations which may access or perform operations
 -- | on the native platform.
@@ -14,3 +15,5 @@ foreign import data PLATFORM :: !
 -- | Register a React Native component from a given `ReactClass` with a name
 -- | so it can be accessed from the native platform.
 foreign import registerComponent :: ∀ eff props. String -> ReactClass props -> Eff (platform :: PLATFORM | eff) Unit
+
+foreign import createElement :: ∀ props. ReactClass props -> props -> Array ReactElement -> ReactElement
