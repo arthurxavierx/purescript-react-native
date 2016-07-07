3f9dfc212845cce777f236ae0e7f08d546154232
Arthur Xavier <arthur.xavierx@gmail.com>
2016-07-07 17:31:24 -0300

    docs(React.Native.Components): add documentation to React.Native.Components module and `view` functi

---
+++ docs/React/Native/Components.md
@@ -0,0 +1,12 @@
+## Module React.Native.Components
+
+#### `view`
+
+``` purescript
+view :: Array Props -> Array ReactElement -> ReactElement
+```
+
+Create a `View` element from an array of `Props` and an array
+of children `ReactElement`.
+
+
--- src/React/Native/Components.purs
+++ src/React/Native/Components.purs
@@ -8,5 +8,7 @@ import React.Native (createElement)
 
 foreign import viewClass :: ∀ props. ReactClass props
 
+-- | Create a `View` element from an array of `Props` and an array
+-- | of children `ReactElement`.
 view :: Array Props -> Array ReactElement -> ReactElement
 view = createElement viewClass
