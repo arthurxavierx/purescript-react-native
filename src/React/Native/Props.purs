c5e0568124ebed5c2ab25dd5945dbd898bf47a9e
Arthur Xavier <arthur.xavierx@gmail.com>
2016-07-23 19:46:29 -0300

    feat(React.Native.Props): add props for `TextInput` component

--- src/React/Native/Props.purs
+++ src/React/Native/Props.purs
@@ -1,6 +1,7 @@
 module React.Native.Props where
 
 import Prelude
+import Color (toHexString, Color)
 import React.DOM.Props (Props, unsafeMkProps)
 
 -- General
@@ -78,3 +79,106 @@ source (SourceUri uri) = unsafeMkProps "source" {uri: uri}
 
 blurRadius :: Number -> Props
 blurRadius = unsafeMkProps "blurRadius"
+
+-- TextInput
+data AutoCapitalize
+  = AutoCapitalizeNone
+  | AutoCapitalizeSentences
+  | AutoCapitalizeWords
+  | AutoCapitalizeCharacters
+
+instance showAutoCapitalize :: Show AutoCapitalize where
+  show AutoCapitalizeNone = "none"
+  show AutoCapitalizeSentences = "sentences"
+  show AutoCapitalizeWords = "words"
+  show AutoCapitalizeCharacters = "characters"
+
+autoCapitalize :: AutoCapitalize -> Props
+autoCapitalize = unsafeMkProps "autoCapitalize" <<< show
+
+autoCorrect :: Boolean -> Props
+autoCorrect = unsafeMkProps "autoCorrect"
+
+autoFocus :: Boolean -> Props
+autoFocus = unsafeMkProps "autoFocus"
+
+blurOnSubmit :: Boolean -> Props
+blurOnSubmit = unsafeMkProps "blurOnSubmit"
+
+defaultValue :: String -> Props
+defaultValue = unsafeMkProps "defaultValue"
+
+editable :: Boolean -> Props
+editable = unsafeMkProps "editable"
+
+maxLength :: Int -> Props
+maxLength = unsafeMkProps "maxLength"
+
+multiline :: Boolean -> Props
+multiline = unsafeMkProps "multiline"
+
+placeholder :: String -> Props
+placeholder = unsafeMkProps "placeholder"
+
+placeholderTextColor :: Color -> Props
+placeholderTextColor = unsafeMkProps "placeholderTextColor" <<< toHexString
+
+secureTextEntry :: Boolean -> Props
+secureTextEntry = unsafeMkProps "secureTextEntry"
+
+selectTextOnFocus :: Boolean -> Props
+selectTextOnFocus = unsafeMkProps "selectTextOnFocus"
+
+selectionColor :: Color -> Props
+selectionColor = unsafeMkProps "selectionColor" <<< toHexString
+
+value :: String -> Props
+value = unsafeMkProps "value"
+
+data KeyboardType
+  = KeyboardDefault
+  | KeyboardEmailAddress
+  | KeyboardNumeric
+  | KeyboardPhonePad
+  | KeyboardAsciiCapable
+  | KeyboardNumbers
+  | KeyboardUrl
+  | KeyboardNumberPad
+  | KeyboardNamePhonePad
+  | KeyboardDecimalPad
+  | KeyboardTwitter
+  | KeyboardWebSearch
+
+instance showKeyboardType :: Show KeyboardType where
+  show KeyboardDefault = "default"
+  show KeyboardEmailAddress = "email-address"
+  show KeyboardNumeric = "numeric"
+  show KeyboardPhonePad = "phone-pad"
+  show KeyboardAsciiCapable = "ascii-capable"
+  show KeyboardNumbers = "numbers-and-punctuation"
+  show KeyboardUrl = "url"
+  show KeyboardNumberPad = "number-pad"
+  show KeyboardNamePhonePad = "name-phone-pad"
+  show KeyboardDecimalPad = "decimal-pad"
+  show KeyboardTwitter = "twitter"
+  show KeyboardWebSearch = "web-search"
+
+keyboardType :: KeyboardType -> Props
+keyboardType = unsafeMkProps "keyboardType" <<< show
+
+data ReturnKeyType
+  = ReturnKeyDone
+  | ReturnKeyGo
+  | ReturnKeyNext
+  | ReturnKeySearch
+  | ReturnKeySend
+
+instance showReturnKeyType :: Show ReturnKeyType where
+  show ReturnKeyDone = "done"
+  show ReturnKeyGo = "go"
+  show ReturnKeyNext = "next"
+  show ReturnKeySearch = "search"
+  show ReturnKeySend = "send"
+
+returnKeyType :: ReturnKeyType -> Props
+returnKeyType = unsafeMkProps "returnKeyType" <<< show
